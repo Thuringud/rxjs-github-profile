7f82847fb3dc19df89160396607745bdb873a4bc
Thuringud <rom1234593@yandex.ru>
2022-10-22 19:23:09 +0600

    first commit

---
+++ src/index.js
@@ -0,0 +1,39 @@
+import { catchError, EMPTY, fromEvent } from 'rxjs';
+import {
+  map, debounceTime, distinctUntilChanged, switchMap, mergeMap, tap, filter,
+} from 'rxjs/operators';
+import { ajax } from 'rxjs/ajax';
+
+const URL = 'https://api.github.com/search/users?q=';
+
+const search = document.getElementById('search');
+const result = document.getElementById('result');
+
+const stream$ = fromEvent(search, 'input')
+  .pipe(
+    map((event) => event.target.value),
+    debounceTime(1000),
+    distinctUntilChanged(),
+    tap(() => result.innerHTML = ''),
+    filter((value) => value.trim()),
+    switchMap((value) => ajax.getJSON(URL + value).pipe(
+      catchError(() => EMPTY),
+    )),
+    map((response) => response.items),
+    mergeMap((items) => items),
+  );
+
+stream$.subscribe((user) => {
+  const html = `
+  <div class="card">
+    <div class="card-image">
+      <img src="${user.avatar_url}" alt="${user.login}">
+      <span class="card-title">${user.login}</span>
+    </div>
+    <div class="card-action">
+      <a href="${user.html_url}" target="_blank">Открыть GitHub</a>
+    </div>
+  </div>
+  `;
+  result.insertAdjacentHTML('beforeend', html);
+});
